78ff183429eb6d6a6755e75e1dc80baccabe3eef
Quan Nguyen <qn@mind-business.com>
2020-01-13 03:01:24 +0100

    change test batch file

--- batch/dme-test-no_extra.sh
+++ batch/dme-test-no_extra.sh
@@ -4,14 +4,16 @@
 #SBATCH --mincpus=8         
 #SBATCH --ntasks=1         
 #SBATCH --cpus-per-task=1
-#SBATCH --time=16:00:00
 #SBATCH -J "dme-t-no_extras"
 #SBATCH --output=/scratch/ws/trng859b-dme/log-%j-no_extras.out
-#SBATCH --mem=16192
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:1           # use 1 GPU per node (i.e. use one GPUper task)
+#SBATCH -p gpu2
+#SBATCH --mem=14192
 
 
 OUTFILE=""
 
-srun python3 main.py with dme.evenly_distributed=True 'dme.extras=["no-extras"]' dme.test_all=False dme.use_validation=True
+srun python3 main.py with dme.evenly_distributed=True 'dme.extras=["no-extras"]' dme.test_all=False dme.use_validation=True dme.num_extra=1
 
 exit 0
