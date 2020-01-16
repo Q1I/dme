4c423963fa2f1618af099bd3b322d147b9ce0398
Quan Nguyen <qn@mind-business.com>
2020-01-16 16:34:12 +0100

    add gpu batch script

---
+++ batch/dme-no_extra-even-gpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A p_scads            
+#SBATCH --nodes=1            
+#SBATCH --mincpus=1         
+#SBATCH --ntasks=1         
+#SBATCH --cpus-per-task=1
+#SBATCH --time=4:00:00
+#SBATCH -J "dme-no_extra"
+#SBATCH --output=/scratch/ws/trng859b-dme/experiment-master-%j-no_extra-even.out
+#SBATCH --gres=gpu:1           # use 1 GPU per node (i.e. use one GPUper task)
+#SBATCH -p gpu2
+#SBATCH --mem=14192
+
+
+OUTFILE=""
+
+srun python3 main.py with dme.evenly_distributed=True "dme.extras=['no-extras']" dme.num_extra=1
+
+exit 0
