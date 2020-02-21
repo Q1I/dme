157a448f6ef350f1947d42a6e2a2c49f53f2f4c6
Quan Nguyen <qn@mind-business.com>
2020-02-21 04:22:24 +0100

    batch fix

--- batch/mv-bcva_delta.sh
+++ batch/mv-bcva_delta.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #SBATCH -A p_scads            
 #SBATCH --nodes=1            
-#SBATCH --mincpus=1         
+#SBATCH --mincpus=18        
 #SBATCH --ntasks=1         
 #SBATCH --cpus-per-task=1
-#SBATCH --time=4:00:00
+#SBATCH --time=6:30:00
 #SBATCH -J "mv-bcva_delta"
 #SBATCH --output=/scratch/ws/trng859b-dme/experiment-mv-%j-bcva_delta.out
 #SBATCH --mem=34192
 
-
 OUTFILE=""
 
-#srun python3 missing_values_run.py with missing_values.evenly_distributed=False "missing_values.extras=['hba1c']" missing_values.num_extra=1
 srun python3 missing_values_run.py
 exit 0
