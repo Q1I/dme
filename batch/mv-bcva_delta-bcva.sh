7cdc8a892cdd343b0e72900e9f4730fb2784b7f7
Quan Nguyen <qn@mind-business.com>
2020-02-26 11:56:24 +0100

    fix batches

--- batch/mv-bcva_delta-bcva.sh
+++ batch/mv-bcva_delta-bcva.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=1         
 #SBATCH --cpus-per-task=1
 #SBATCH --time=6:30:00
-#SBATCH -J "mv-bcva_delta"
+#SBATCH -J "mv-bcva_delta-bcva"
 #SBATCH --output=/scratch/ws/trng859b-dme/experiment-mv-%j-bcva_delta-bcva.out
 #SBATCH --mem=34192
 
 OUTFILE=""
 
-srun python3 missing_values_run.py with "missing_values.extras=['bcva']" missing_values.num_extra=1
+srun python3 missing_values_run.py with "missing_values.extras=['bcva_delta_m0_m12','bcva']" missing_values.num_extra=2
 exit 0
--- batch/mv-bcva_delta-misc.sh
+++ batch/mv-bcva_delta-misc.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=1         
 #SBATCH --cpus-per-task=1
 #SBATCH --time=6:30:00
-#SBATCH -J "mv-bcva_delta-bcva"
+#SBATCH -J "mv-bcva_delta-misc"
 #SBATCH --output=/scratch/ws/trng859b-dme/experiment-mv-%j-bcva_delta-misc.out
 #SBATCH --mem=34192
 
 OUTFILE=""
 
-srun python3 missing_values_run.py with "missing_values.extras=['bcva', 'age', 'avegf_ranibizumab', 'avegf_aflibercept', 'avegf_bevacizumab']"
+srun python3 missing_values_run.py with "missing_values.extras=['bcva_delta_m0_m12', bcva', 'age', 'avegf_ranibizumab', 'avegf_aflibercept', 'avegf_bevacizumab']"
 exit 0
